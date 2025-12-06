77e932f76890f9d2b67127479db1442762beef43
Anastasia Grechanichenko <anastasiagrecanicenko204@gmail.com>
2025-12-06 15:27:19 +0300

    версия2

--- TaskManager.py
+++ TaskManager.py
@@ -1,14 +1,44 @@
 class TaskManager:
     tasks = []
+    path = "cache.csv"
+
+    def __init__(self):
+        self.load_data()
+
+    def save_data(self):
+        with open(self.path,"w",encoding ="utf-8") as file:
+            for task in self.tasks:
+                file.write(f"{task['name']}:{task['description']}\n")
+                '''Каждая task — это словарь вида: {'name': 'Купить молоко', 'description': 'Из магазина'}
+                '''
+
+    def load_data(self):
+        try:
+            with open(self.path,"r", encoding="utf-8")as file:
+                contents = file.readlines()
+                if contents =="":
+                    return
+                for task in contents:
+                    values = task.split(":")
+                    self.create_task(values[0],values[1])
+        except FileNotFoundError:
+            print("файл не найден")
+
+
+
 
     def create_task(self,name,description):
         self.tasks.append({"name" : name,"description" : description}) # slovar
+        self.save_data()
 
-    def get_str_tasks(self): # смысл (назначение) заключается в преобразовании списка задач в единую читаемую строку.
+
+    def get_str_tasks(self):
+        if not self.tasks:# смысл (назначение) заключается в преобразовании списка задач в единую читаемую строку.
+            return "нет задач"
+
         str_tasks = "" # Инициализирует пустую строку str_tasks
-        for i in self.tasks: # Перебирает каждую задачу (i) в списке self.tasks
-            str_tasks += f"{i['name']}: {i['description']}\n" """Форматирует каждую задачу в строку вида "Название 
-            задачи:Описание задачи", добавляет перевод строки (\n) и присоединяет результат к общей строке str_tasks"""
+        for task in self.tasks: # Перебирает каждую задачу (i) в списке self.tasks
+            str_tasks += f"{task['name']}: {task['description']}\n"
         return str_tasks
 
 
--- main.py
+++ main.py
@@ -1,41 +1,72 @@
 import telebot
+from telebot import types
 from TaskManager import TaskManager
 
-token='8340772869:AAFjDgh_U9NuODQxh0OdmGw8r1a65haOdYY'
+
+token= ('8593774051:AAHOi9kCXGdlXmTX0DrbByaMkHTHn_jb-fg')
 bot=telebot.TeleBot(token) #bot — это экземпляр класса TeleBot из библиотеки pyTelegramBotAPI.
 
 taskManager = TaskManager()
 
-cache = {} '''cache = {
-    123456789: "ожидание названия задачи", 
-    987654321: "ожидание описания задачи"
-}
-где 123456789 и 987654321 — это user_id
-словарь, ключ юзер айди
-'''
+cache = {}
 
+def check_create_button(message):
+    return message.text =="Создать задачу"
+
+def check_show_button(message):
+    return message.text == "Показать задачи"
+
+
+def main_menu():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_create = types.KeyboardButton("Создать задачу")
+    button_show = types.KeyboardButton("Показать задачи")
+    markup.add(button_create,button_show)
+    return markup
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id,"Привет ✌️ ")
+    bot.send_message(message.chat.id,"Привет ✌️. Выбери действие: ",reply_markup=main_menu()
+                     )
 
+@bot.message_handler(func=check_create_button)
+def handle_create_button(message):
+    create_task(message)
 
-# начало создания задачи
-@bot.message_handler(commands=['create'])
-'''
-"Если пользователь отправляет команду /create, то выполни функцию create_task."
-'''
+@bot.message_handler(func=check_show_button)
+def handle_show_button(message):
+    start_message_show(message)
+
+@bot.message_handler(commands=['show'])
+def start_message_show(message):
+    tasks = taskManager.get_str_tasks()
+    bot.send_message(message.chat.id, f"{tasks}")
+
+@bot.message_handler(commands=['create']) #Если пользователь отправляет команду /create, то выполни функцию create_task
 def create_task(message):
     chat_id = message.chat.id
     cache [str(chat_id)] = {} #chat_id для запоминания конкретного пользователя
     msg = bot.reply_to(message,"Введите название задачи ✌️ ")
     bot.register_next_step_handler(msg,process_name)
-def get_task_name(message):
-    task_name = message.text
 
-@bot.message_handler(commands=['show'])
-def start_message(message):
-    tasks = taskManager.get_tasks()
-    bot.send_message(message.chat.id,f"{tasks}")
+def process_name(message):
+    try:
+        chat_id = message.chat.id
+        cache[str(chat_id)]['name'] = message.text
+        msg = bot.reply_to(message,"Введите описание задачи")
+        bot.register_next_step_handler(msg,process_desc)
+    except Exception as ex:
+        bot.reply_to(message,"ошибочка")
+
+
+def process_desc(message):
+    try:
+        chat_id = message.chat.id
+        cache[str(chat_id)]['description'] = message.text
+        taskManager.create_task(cache[str(chat_id)]['name'],cache[str(chat_id)]['description'])
+        bot.reply_to(message,"Задача успешно добавлена!")
+    except Exception as ex:
+        bot.reply_to(message,"что-то не так")
+
 
 bot.infinity_polling() # Это вечный цикл, который постоянно опрашивает серверы Telegram на наличие новых сообщений.
